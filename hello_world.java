c488dd1064049b5ab2fc03f4169ff5eade696404
leedy1999 <2011684909@qq.com>
2021-04-30 00:54:05 +0800

    My first commit

---
+++ hello_world.java
@@ -0,0 +1,9 @@
+public class hello_world {
+    public static void main() {
+        System.out.println(helloWorld());
+    }
+
+    public static String helloWorld() {
+        return "Hello World";
+    }
+}
